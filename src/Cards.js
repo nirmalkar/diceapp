2ed5af9e4fe119175300397e0abe5273260aecf2
hemantkumar2 <hemantnirmalkar17@gmail.com>
2019-12-02 18:47:48 +0530

    timeout added

---
+++ src/Cards.js
@@ -0,0 +1,14 @@
+
+
+import React, { Component } from 'react'
+
+export default class Cards extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Cards</h1>
+
+      </div>
+    )
+  }
+}
--- src/DiceRoll.js
+++ src/DiceRoll.js
@@ -11,14 +11,20 @@ export default class DiceRoll extends Component {
 
     this.state = {
       die1: 'one',
-      die2: 'one'
+      die2: 'one',
+      isRolling: false
     }
     this.roll = this.roll.bind(this)
   }
   roll() {
     const forDieOne = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
     const forDieTwo = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
-    this.setState({ die1: forDieOne, die2: forDieTwo })
+    this.setState({ die1: forDieOne, die2: forDieTwo, isRolling: true })
+
+    // wait one second, then set rolling to false
+    setTimeout(() => {
+      this.setState({ isRolling: false })
+    }, 1000)
   }
 
   render() {
@@ -28,7 +34,9 @@ export default class DiceRoll extends Component {
           <Dice face={this.state.die1} />&nbsp;&nbsp;&nbsp;&nbsp;
           <Dice face={this.state.die2} />
         </div>
-        <button onClick={this.roll} className='btn btn-dark btn-lg mt-3'>Roll Dice!</button>
+        <button onClick={this.roll} className='btn btn-dark btn-lg mt-3'>
+          {this.state.isRolling ? 'Rolling...' : 'Roll Dice'}
+        </button>
       </div>
     )
   }
